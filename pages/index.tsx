342c1d2be93717772a5802949731363427e98304
a6Ce6Bs <perodyz@protonmail.com>
2022-09-07 14:38:09 +0200

    add: Seo component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,13 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
 import Hero from 'components/Hero'
 import Details from 'components/Details'
 import Community from 'components/Community'
+import Seo from 'components/Seo'
 
 const Home: NextPage = () => {
   return (
     <div>
-      <Head>
-        <title>Home | ERC Wizard Protocol</title>
-        <meta
-          name="description"
-          content="Generate ERC Smart Contracts with No-Code for NFT's Next Project"
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
+      <Seo />
       <main>
         <Hero />
         <Details />
