4b56d798a5e79952193d96f2a6a1f65625d36d78
a6Ce6Bs <perodyz@protonmail.com>
2022-09-08 16:53:25 +0200

    add: wagmi library

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,62 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { WagmiConfig, createClient, chain, configureChains } from 'wagmi'
+import { alchemyProvider } from 'wagmi/providers/alchemy'
+import { publicProvider } from 'wagmi/providers/public'
+import { CoinbaseWalletConnector } from 'wagmi/connectors/coinbaseWallet'
+import { InjectedConnector } from 'wagmi/connectors/injected'
+import { MetaMaskConnector } from 'wagmi/connectors/metaMask'
+import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
 import Header from 'components/Header'
 import Footer from 'components/Footer'
 
+const { chains, provider, webSocketProvider } = configureChains(
+  [chain.polygon, chain.polygonMumbai, chain.hardhat],
+  [
+    alchemyProvider({
+      apiKey: process.env.NEXT_PUBLIC_POLYGON_ALCHEMY_API_KEY ?? '',
+    }),
+    publicProvider(),
+  ]
+)
+
+const client = createClient({
+  autoConnect: true,
+  connectors: [
+    new MetaMaskConnector({ chains }),
+    new CoinbaseWalletConnector({
+      chains,
+      options: {
+        appName: 'ERC Wizard',
+      },
+    }),
+    new WalletConnectConnector({
+      chains,
+      options: {
+        qrcode: true,
+      },
+    }),
+    new InjectedConnector({
+      chains,
+      options: {
+        name: 'Injected',
+        shimDisconnect: true,
+      },
+    }),
+  ],
+  provider,
+  webSocketProvider,
+})
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div className="bg-black text-white px-8 sm:px-16">
-      <Header />
-      <Component {...pageProps} />
-      <Footer />
-    </div>
+    <WagmiConfig client={client}>
+      <div className="bg-black text-white px-8 sm:px-16">
+        <Header />
+        <Component {...pageProps} />
+        <Footer />
+      </div>
+    </WagmiConfig>
   )
 }
 
