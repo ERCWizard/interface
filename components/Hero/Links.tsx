b2e1d088d918b29a0cf22ca20e5a2894ad2eb1df
a6Ce6Bs <perodyz@protonmail.com>
2022-09-09 19:10:45 +0200

    update: Links component & deco.svg

--- components/Hero/Links.tsx
+++ components/Hero/Links.tsx
@@ -4,39 +4,38 @@ import { twitterHref, discordHref, githubHref } from 'utils/constants'
 import { factoryAddresses } from 'constants/addresses'
 import { SupportedChainId } from 'constants/chains'
 
+const style = {
+  main: `flex justify-between gap-8 w-full mt-16`,
+  polygon: `border border-neutral-900 h-12 sm:h-16 flex items-center justify-center hover:bg-neutral-900 transition duration-200 ease-in-out`,
+  polygonSmall: `flex items-center justify-center sm:hidden w-12 sm:w-16`,
+  socials: `flex space-x-8 sm:space-x-12`,
+  social: `border border-neutral-900 w-12 h-12 sm:w-16 sm:h-16 flex items-center justify-center hover:bg-neutral-900 transition duration-200 ease-in-out`,
+}
+
 export default function Links() {
   return (
-    <div className="flex justify-between gap-8 w-full mt-16">
+    <div className={style.main}>
       <a
         href={`https://polygonscan.com/address/${
           factoryAddresses[SupportedChainId.POLYGON]
         }`}
-        className="border h-12 sm:h-16 flex items-center justify-center hover:bg-neutral-900 transition duration-200 ease-in-out"
+        className={style.polygon}
       >
         <div className="hidden sm:flex">
           <Image src={polygonLogo} height={32} alt="polygon logo" />
         </div>
-        <div className="flex items-center justify-center sm:hidden w-12 sm:w-16">
+        <div className={style.polygonSmall}>
           <Image src={polygon} height={32} width={32} alt="polygon logo" />
         </div>
       </a>
-      <div className="flex space-x-8 sm:space-x-12">
-        <a
-          href={twitterHref}
-          className="border w-12 h-12 sm:w-16 sm:h-16 flex items-center justify-center hover:bg-neutral-900 transition duration-200 ease-in-out"
-        >
+      <div className={style.socials}>
+        <a href={twitterHref} className={style.social}>
           <Image src={twitter} width={32} height={32} alt="twitter logo" />
         </a>
-        <a
-          href={discordHref}
-          className="border w-12 h-12 sm:w-16 sm:h-16 flex items-center justify-center hover:bg-neutral-900 transition duration-200 ease-in-out"
-        >
+        <a href={discordHref} className={style.social}>
           <Image src={discord} width={32} height={32} alt="discord logo" />
         </a>
-        <a
-          href={githubHref}
-          className="border w-12 h-12 sm:w-16 sm:h-16 flex items-center justify-center hover:bg-neutral-900 transition duration-200 ease-in-out"
-        >
+        <a href={githubHref} className={style.social}>
           <Image src={github} width={32} height={32} alt="github logo" />
         </a>
       </div>
