7a158c9b2bae417ebdcb77337301c49e5d463611
a6Ce6Bs <perodyz@protonmail.com>
2022-09-16 11:30:48 +0200

    update: cypress.config & ethereum provider mock

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'cypress'
+import 'dotenv/config'
 
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      config.env = {
+        ...process.env,
+        ...config.env,
+      }
+      return config
     },
     specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
   },
--- cypress/support/ethereum.ts
+++ cypress/support/ethereum.ts
@@ -4,30 +4,36 @@
 import { Eip1193Bridge } from '@ethersproject/experimental/lib/eip1193-bridge'
 import { JsonRpcProvider } from '@ethersproject/providers'
 import { Wallet } from '@ethersproject/wallet'
+import { ethers } from 'ethers'
+import { formatAddress } from '../../utils/formatAddress'
 
 /**
  * hardhat  id 31337 | hex 0x7A69
  * mumbai   id 80001 | hex 0x13881
  */
-const chainId = 80001
-const chainIdHex = '0x13881'
+const chainId = 31337
+const chainIdHex = '0x7A69'
 
 // hardhat prive key
-const TEST_PRIVATE_KEY = `0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80`
+const TEST_PRIVATE_KEY = Cypress.env('TEST_PRIVATE_KEY')
 
 // address of the above key
 export const TEST_ADDRESS_NEVER_USE = new Wallet(TEST_PRIVATE_KEY).address
 
 // format wallet address
-export const TEST_ADDRESS_NEVER_USE_SHORTENED = `${TEST_ADDRESS_NEVER_USE.substr(
-  0,
-  6
-)}...${TEST_ADDRESS_NEVER_USE.substr(-4, 4)}`
-
-const provider = new JsonRpcProvider(
-  `https://polygon-mumbai.g.alchemy.com/v2/${process.env.POLYGON_ALCHEMY_API_KEY}`,
-  chainId
+export const TEST_ADDRESS_NEVER_USE_SHORTENED = formatAddress(
+  TEST_ADDRESS_NEVER_USE
 )
+
+const rpcProvider =
+  chainId === 31337
+    ? 'http://127.0.0.1:8545'
+    : `https://polygon-mumbai.g.alchemy.com/v2/${Cypress.env(
+        'POLYGON_ALCHEMY_API_KEY'
+      )}`
+
+const provider = new JsonRpcProvider(rpcProvider, chainId)
+
 const signer = new Wallet(TEST_PRIVATE_KEY, provider)
 
 export const injected = new (class extends Eip1193Bridge {
@@ -52,6 +58,7 @@ export const injected = new (class extends Eip1193Bridge {
       method = args[0]
       params = args[1]
     }
+    // Mock out request accounts and chainId
     if (method === 'eth_requestAccounts' || method === 'eth_accounts') {
       if (isCallbackForm) {
         callback({ result: [TEST_ADDRESS_NEVER_USE] })
@@ -67,7 +74,41 @@ export const injected = new (class extends Eip1193Bridge {
       }
     }
     try {
-      const result = await super.send(method, params)
+      // If from is present on eth_call it errors, removing it makes the library set
+      // from as the connected wallet which works fine
+      if (params && params.length && params[0].from && method === 'eth_call')
+        delete params[0].from
+      let result
+      // For sending a transaction if we call send it will error
+      // as it wants gasLimit in sendTransaction but hexlify sets the property gas
+      // to gasLimit which makes sensd transaction error.
+      // This have taken the code from the super method for sendTransaction and altered
+      // it slightly to make it work with the gas limit issues.
+      if (
+        params &&
+        params.length &&
+        params[0].from &&
+        method === 'eth_sendTransaction'
+      ) {
+        // Hexlify will not take gas, must be gasLimit, set this property to be gasLimit
+        params[0].gasLimit = params[0].gas
+        delete params[0].gas
+        // If from is present on eth_sendTransaction it errors, removing it makes the library set
+        // from as the connected wallet which works fine
+        delete params[0].from
+        const req = ethers.providers.JsonRpcProvider.hexlifyTransaction(
+          params[0]
+        )
+        // Hexlify sets the gasLimit property to be gas again and send transaction requires gasLimit
+        req.gasLimit = req.gas
+        delete req.gas
+        // Send the transaction
+        const tx = await this.signer.sendTransaction(req)
+        result = tx.hash
+      } else {
+        // All other transactions the base class works for
+        result = await super.send(method, params)
+      }
       console.debug('result received', method, params, result)
       if (isCallbackForm) {
         callback(null, { result })
@@ -75,6 +116,7 @@ export const injected = new (class extends Eip1193Bridge {
         return result
       }
     } catch (error) {
+      console.log(error)
       if (isCallbackForm) {
         callback(error, null)
       } else {
